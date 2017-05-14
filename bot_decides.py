28cf6d784731c3d3a58f733118cae3126e7db480
Daniel Killenberger <daniel.killenbeger@gmail.com>
2017-05-14 20:35:23 +0200

    Initial commit

---
+++ bot_decides.py
@@ -0,0 +1,81 @@
+import json
+import os
+import random
+from telegram.ext import Updater
+from telegram.ext import CommandHandler
+
+DIR = os.path.dirname(os.path.abspath(__file__))
+
+config = open(os.path.join(DIR, "telegram_config.json")).read()
+config = json.loads(config)
+telegram_token = config['telegram_api_token']
+updater = Updater(token=telegram_token)
+dispatcher = updater.dispatcher
+
+
+def answer(bot, update, message):
+    bot.send_message(chat_id=update.message.chat_id, text=message)
+
+
+def roll_error_response(bot, update):
+    answer(bot, update, 'Without parameters: Ask for a roll between 1-10 \n'
+                        'With integer n > 2 as a parameter:\n'
+                        'Roll between 1-n')
+
+
+def roll_response(bot, update, n):
+    number = random_roll(n)
+    answer(bot, update, 'Roll between 1-{n}: {number}'.format(**locals()))
+
+
+def random_roll(n):
+    return random.randint(0, n)
+
+
+def start(bot, update):
+    answer(bot, update, 'You can roll random numbers with /roll n where n is an optional integer parameter'
+                        ' that has to be bigger than 2.\n'
+                        'You can choose between multiple options by entering /choose option1, option2, ... optionN\n')
+
+
+def roll(bot, update):
+    message = update.message.text.replace("/roll", "").strip()
+    if len(message) > 0:
+        try:
+            n = int(message)
+        except ValueError as err:
+            roll_error_response(bot, update)
+        if n < 2:
+            roll_error_response(bot, update)
+        else:
+            number = random_roll(n)
+            roll_response(bot, update, n)
+    else:
+        roll_response(bot, update, 10)
+
+
+def choose(bot, update):
+    message = update.message.text.replace("/choose", "").replace(" ", "").strip()
+
+    if len(message) > 0:
+        choices = message.split(',')
+        i = random_roll(len(choices)-1)
+        response = "Choices: \n"
+        for choice in choices:
+            response += '{choice} \n'.format(**locals())
+
+        choice = choices[i]
+        response += '\nI have chosen the following: {choice}'.format(**locals())
+        answer(bot, update, response)
+    else:
+        answer(bot, update, 'You need to add choices separated by commas')
+
+
+start_handler = CommandHandler('start', start)
+roll_handler = CommandHandler('roll', roll)
+choose_handler = CommandHandler('choose', choose)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(roll_handler)
+dispatcher.add_handler(choose_handler)
+
+updater.start_polling()
